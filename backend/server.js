46d2e9c7fbaa812e38928d397ff44ee7e4366a55
Shavon Harris <Shavonharris9114@gmail.com>
2022-05-13 21:19:38 -0700

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Lyrics')
+const express = require('express')
+const dotenv = require('dotenv').config
+const port = process.env.PORT || 5000
+
+
+const app = express()
+
+app.use('/api/songs', require('./routes/songRoutes'))
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
